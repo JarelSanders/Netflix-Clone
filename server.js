d3722e67bea2051d3afcd85f2ae6e04adb2d20b3
Jarel Sanders <jarelsanders1986@gmail.com>
2024-09-01 10:29:44 -0400

    Set up production build and express server

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path')
+const app = express()
+
+// Serve the static files from the dist directory
+app.use(express.static(__dirname + '/dist/netflix-clone'));
+
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/netflix-clone/index.html'));
+});
+
+
+
+app.listen(8080);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
